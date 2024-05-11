01972ded2b4ad2b7f661dcae813b5f04611c5761
lqin61 <lqin61@gatech.edu>
2024-05-11 14:30:56 -0500

    start working

--- src/router.ts
+++ src/router.ts
@@ -39,94 +39,94 @@ const router = new Router({
     {
       path: "/",
       component: Layout,
-      redirect: "/dashboard",
+      redirect: "/NPS",
       children: [
-        {
-          path: "dashboard",
-          component: () =>  import("@/views/dashboard/index.vue"),
-          name: "Dashboard",
-          meta: {
-            title: "工作台",
-            icon: "dashboard",
-            affix: true
-          }
-        },
-		    {
-          path: "/statistics",
-          component: () => import("@/views/statistics/index.vue"),
-          meta: {
-            title: "数据统计",
-            icon: "icon-statistics"
-          }
-        },
-        {
-          path: "order",
-          component: () => import("@/views/orderDetails/index.vue"),
-          meta: {
-            title: "订单管理",
-            icon: "icon-order"
-          }
-        },
-        {
-          path: "setmeal",
-          component: () => import("@/views/setmeal/index.vue"),
-          meta: {
-            title: "套餐管理",
-            icon: "icon-combo"
-          }
-        },
-        {
-          path: "dish",
-          component: () => import("@/views/dish/index.vue"),
-          meta: {
-            title: "菜品管理",
-            icon: "icon-dish"
-          }
-        },
-        {
-          path: "/dish/add",
-          component: () => import("@/views/dish/addDishtype.vue"),
-          meta: {
-            title: "添加菜品",
-            hidden: true
-          }
-        },
+        // {
+        //   path: "dashboard",
+        //   component: () =>  import("@/views/dashboard/index.vue"),
+        //   name: "Dashboard",
+        //   meta: {
+        //     title: "工作台",
+        //     icon: "dashboard",
+        //     affix: true
+        //   }
+        // },
+		    // {
+        //   path: "/statistics",
+        //   component: () => import("@/views/statistics/index.vue"),
+        //   meta: {
+        //     title: "数据统计",
+        //     icon: "icon-statistics"
+        //   }
+        // },
+        // {
+        //   path: "order",
+        //   component: () => import("@/views/orderDetails/index.vue"),
+        //   meta: {
+        //     title: "订单管理",
+        //     icon: "icon-order"
+        //   }
+        // },
+        // {
+        //   path: "setmeal",
+        //   component: () => import("@/views/setmeal/index.vue"),
+        //   meta: {
+        //     title: "套餐管理",
+        //     icon: "icon-combo"
+        //   }
+        // },
+        // {
+        //   path: "dish",
+        //   component: () => import("@/views/dish/index.vue"),
+        //   meta: {
+        //     title: "菜品管理",
+        //     icon: "icon-dish"
+        //   }
+        // },
+        // {
+        //   path: "/dish/add",
+        //   component: () => import("@/views/dish/addDishtype.vue"),
+        //   meta: {
+        //     title: "添加菜品",
+        //     hidden: true
+        //   }
+        // },
         
-        {
-          path: "category",
-          component: () => import("@/views/category/index.vue"),
-          meta: {
-            title: "分类管理",
-            icon: "icon-category"
-          }
-        },
-        {
-          path: "employee",
-          component: () => import("@/views/employee/index.vue"),
-          meta: {
-            title: "员工管理",
-            icon: "icon-employee"
-          }
+        // {
+        //   path: "category",
+        //   component: () => import("@/views/category/index.vue"),
+        //   meta: {
+        //     title: "分类管理",
+        //     icon: "icon-category"
+        //   }
+        // },
+        // {
+        //   path: "employee",
+        //   component: () => import("@/views/employee/index.vue"),
+        //   meta: {
+        //     title: "员工管理",
+        //     icon: "icon-employee"
+        //   }
 
-        },
+        // },
         
-        {
-          path: "/employee/add",
-          component: () => import("@/views/employee/addEmployee.vue"),
-          meta: {
-            title: "添加/修改员工",
-            hidden: true
-          }
-        },
+        // {
+        //   path: "/employee/add",
+        //   component: () => import("@/views/employee/addEmployee.vue"),
+        //   meta: {
+        //     title: "添加/修改员工",
+        //     hidden: true
+        //   }
+        // },
         
-        {
-          path: "/setmeal/add",
-          component: () => import("@/views/setmeal/addSetmeal.vue"),
-          meta: {
-            title: "添加套餐",
-            hidden: true
-          }
-        },
+        // {
+        //   path: "/setmeal/add",
+        //   component: () => import("@/views/setmeal/addSetmeal.vue"),
+        //   meta: {
+        //     title: "添加套餐",
+        //     hidden: true
+        //   }
+        // },
 
         {
           path: "NPS",
---
+++ src/views/NPS/npsSearch.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="addBrand-container">
+      <div class="container">
+        <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="180px">
+          <el-form-item label="Username" prop="username">
+            <el-input v-model="ruleForm.username"></el-input>
+          </el-form-item>
+          <el-form-item label="Name" prop="name">
+            <el-input v-model="ruleForm.name"></el-input>
+          </el-form-item>
+          <el-form-item label="Phone" prop="phone">
+            <el-input v-model="ruleForm.phone"></el-input>
+          </el-form-item>
+          <el-form-item label="Sex" prop="sex">
+              <el-radio v-model="ruleForm.sex" label="1">男</el-radio>
+              <el-radio v-model="ruleForm.sex" label="2">女</el-radio>
+          </el-form-item>
+          <el-form-item label="ID" prop="idNumber">
+            <el-input v-model="ruleForm.idNumber"></el-input>
+          </el-form-item>
+          <div class="subBox">
+            <el-button type="primary" @click="submitForm('ruleForm',false)">保存</el-button>
+            <el-button 
+              v-if="this.optType === 'add'" 
+              type="primary" 
+              @click="submitForm('ruleForm',true)">保存并继续添加员工
+            </el-button>
+            <el-button @click="() => this.$router.push('/employee')">返回</el-button>
+          </div>
+        </el-form>
+      </div>
+    </div>
+  </template>
